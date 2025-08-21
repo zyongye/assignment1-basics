78712f9ea37d48090745a8405c25b0d0c8fce83f
Yongye Zhu <zyy1102000@gmail.com>
2025-08-21 11:58:55 -0700

    cos_lr_decay and gradient clipping

--- cs336_basics/util.py
+++ cs336_basics/util.py
@@ -1,3 +1,4 @@
+import math
 import torch
 
 
@@ -15,6 +16,28 @@ def log_softmax(x, dim=-1):
 def cross_entropy(inputs, targets):
     x = -log_softmax(inputs)
     return torch.mean(torch.gather(x, -1, targets.unsqueeze(-1)))
+
+
+def cos_anneling_lr_decay(t, alpha_max, alpha_min, T_w, T_c):
+    if t < T_w:
+        return t / T_w * alpha_max
+    if t > T_c:
+        return alpha_min
+    return alpha_min + (1 + math.cos((t - T_w) / (T_c - T_w) * math.pi)) * (alpha_max - alpha_min) / 2
+
+def gradient_clipping(parameters, max_l2_norm):
+    grads = [p.grad for p in parameters if p.grad is not None]
+    eps = 10e-6
+    norm = 0
+    for g in grads:
+        norm += (g**2).sum()
+    # require norm for all params
+    norm = torch.sqrt(norm)
+    if norm > max_l2_norm:
+        factor = max_l2_norm / (norm + eps)
+        for g in grads:
+            g *= factor
+
 
 
 
--- tests/adapters.py
+++ tests/adapters.py
@@ -507,8 +507,7 @@ def run_gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm:
 
     The gradients of the parameters (parameter.grad) should be modified in-place.
     """
-    raise NotImplementedError
-
+    return util.gradient_clipping(parameters, max_l2_norm)
 
 def get_adamw_cls() -> Any:
     """
@@ -542,7 +541,7 @@ def run_get_lr_cosine_schedule(
     Returns:
         Learning rate at the given iteration under the specified schedule.
     """
-    raise NotImplementedError
+    return util.cos_anneling_lr_decay(it, max_learning_rate, min_learning_rate, warmup_iters, cosine_cycle_iters)
 
 
 def run_save_checkpoint(
